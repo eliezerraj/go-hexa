8b28036973b47f6c6eac5dbddfbecc111d980c24
eliezer antunes <eliezeraj@navis,systems>
2021-12-18 01:47:58 -0300

    commit

--- proto-shared/proto/gen_protoc.sh
+++ proto-shared/proto/gen_protoc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdir -p ../generated/go
-protoc rate/rate.proto --go_out=plugins=grpc:../generated/go
+protoc rate/rate.proto --go_out=plugins=grpc:../../../../
+protoc balance/balance.proto --go_out=plugins=grpc:../../../../
